91f71737322fc82bca1d61eaeb56a2e774baa429
ANIL KOLLA <89354787+S545232@users.noreply.github.com>
2021-11-10 21:53:27 -0600

    unwanted code removed

--- src/flight/Flight.java
+++ src/flight/Flight.java
@@ -128,31 +128,3 @@ public class Flight {
         return;
     }
 }
-
-//public Passenger[] makePassengersList() {
-//        Passenger[] passengersArray = new Passenger[10];
-//
-//        Passenger p1 = new Passenger(1, "DL1655", "Elon1", "New York", "Hyderabad");
-//        Passenger p2 = new Passenger(2, "DL1633", "Elon2", "New York", "Hyderabad");
-//        Passenger p3 = new Passenger(3, "DL1677", "Elon3", "New York", "Hyderabad");
-//        Passenger p4 = new Passenger(4, "DL1655", "Elon4", "New York", "Hyderabad");
-//        Passenger p5 = new Passenger(5, "DL1677", "Elon5", "New York", "Hyderabad");
-//        Passenger p6 = new Passenger(6, "DL1633", "Elon6", "New York", "Hyderabad");
-//        Passenger p7 = new Passenger(7, "DL1655", "Elon7", "New York", "Hyderabad");
-//        Passenger p8 = new Passenger(8, "DL1677", "Elon8", "New York", "Hyderabad");
-//        Passenger p9 = new Passenger(9, "DL1655", "Elon9", "New York", "Hyderabad");
-//        Passenger p10 = new Passenger(10, "DL1633", "Elon10", "New York", "Hyderabad");
-//
-//        passengersArray[0] = p1;
-//        passengersArray[1] = p2;
-//        passengersArray[2] = p3;
-//        passengersArray[3] = p4;
-//        passengersArray[4] = p5;
-//        passengersArray[5] = p6;
-//        passengersArray[6] = p7;
-//        passengersArray[7] = p8;
-//        passengersArray[8] = p9;
-//        passengersArray[9] = p10;
-//
-//        return passengersArray;
-//    }
